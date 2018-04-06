1ac5b270d263ec3d8c1e349a5fc397859e6ced4b
Javier Aragón <javier.aragon@geographica.gs>
2018-04-06 14:48:48 +0200

    buttons and checkboxs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
+import { CheckboxComponent } from './components/checkbox/checkbox.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    CheckboxComponent
   ],
   imports: [
     BrowserModule
