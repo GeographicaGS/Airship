6e7607a6d4bd8e23d3636d059c21923dd3628060
Javier Aragón <javier.aragon@geographica.gs>
2018-04-12 15:42:41 +0200

    datepicker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
+  datePickerValue = new Date('October 13, 2014 11:13:00');
+  dateRangeValue = {
+    start: new Date(),
+    end: new Date(new Date().setDate(new Date().getDate() + 3))
+  };
+
   rageValues = [100, 500];
   rageValue = [200];
+
   rageParseFunction = (value, index) => {
     if (value === 500) {
       return `Above ${500}`;
     }
     return value;
   }
-
-
 
   ngOnInit() {
   }
@@ -26,6 +31,10 @@ export class AppComponent implements OnInit {
   }
 
   valueRangeChange(value) {
+    console.log(value);
+  }
+
+  valueDatePickerChange(value) {
     console.log(value);
   }
 }
